9dd59302c84d499e1a9526687ed3b18697ea93f0
dav3sk <davibastos.v@gmail.com>
2018-06-27 16:47:37 -0300

    Implementado controladores de dentista

--- src/main/java/gohorse/dentalclean/controller/AbstractFacade.java
+++ src/main/java/gohorse/dentalclean/controller/AbstractFacade.java
@@ -1,17 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package gohorse.dentalclean.controller;
 
 import java.util.List;
 import javax.persistence.EntityManager;
 
-/**
- *
- * @author dav3s
- */
 public abstract class AbstractFacade<T> {
 
     private Class<T> entityClass;
---
+++ src/main/java/gohorse/dentalclean/model/entity/Dentista.java
@@ -0,0 +1,168 @@
+package gohorse.dentalclean.model.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+
+@Entity
+@Table(name="dentista")
+public class Dentista implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name="nome",
+            nullable=false,
+            length=100)
+    private String nome;
+    
+    @Column(name="telefone",
+            nullable=true,
+            length=15)
+    private String telefone;
+    
+    @Column(name="email",
+            nullable=false,
+            length=244,
+            unique=true)
+    private String email;
+    
+    @Column(name="cpf",
+            nullable=false,
+            length=14,
+            unique=true)
+    private String cpf;
+    
+    @Column(name="rg",
+            length=12)
+    private String rg;
+    
+    @Column(name="cep",
+            nullable=false,
+            length=9)
+    private String cep;
+    
+    @Column(name="dataDeNascimento",
+            nullable=false)
+    @Temporal(javax.persistence.TemporalType.DATE)
+    private Date dataDeNascimento;
+    
+    @Column(name="nivelAcesso",
+            length=14)
+    private final String nivelAcesso = "DENTISTA";
+    
+    @Column(name="senha",
+            nullable=false,
+            length=35)
+    private String senha;
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getRg() {
+        return rg;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public Date getDataDeNascimento() {
+        return dataDeNascimento;
+    }
+
+    public void setDataDeNascimento(Date dataDeNascimento) {
+        this.dataDeNascimento = dataDeNascimento;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getNivelAcesso() {
+        return nivelAcesso;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Dentista)) {
+            return false;
+        }
+        Dentista other = (Dentista) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "gohorse.dentalclean.model.entity.Dentista[ id=" + id + " ]";
+    }
+    
+}
